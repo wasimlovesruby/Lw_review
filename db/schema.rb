64542b82974aaa4d5f37dffefee1912de3a235b7
wasimlovesruby <wasimraza143@yahoo.com>
2017-06-11 08:11:29 +0500

    reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610100448) do
+ActiveRecord::Schema.define(version: 20170610133950) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170610100448) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
